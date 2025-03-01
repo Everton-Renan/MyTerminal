149e268075d90c392a24473c27a998ff58e46190
Everton-Renan <evertonrenan917@gmail.com>
2025-03-01 01:31:24 -0300

    Create Terminal class with execute_command, show_error_message and run method, Create RunCommands with run_commands method, and set INPUT_COLOR, ERROR_COLOR and RESET_COLOR

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-command = input('MyTerminal (input): ')
+import os
+import subprocess
+
+from myterminal.commands_dict import create_commands
+
+INPUT_COLOR = '\033[32m'
+ERROR_COLOR = '\033[31m'
+RESET_COLOR = '\033[0m'
 
 
 class UnpackingCommands:
@@ -7,3 +14,72 @@ class UnpackingCommands:
 
     def unpacking(self) -> list[str]:
         return command.split(' ')
+
+
+class Terminal:
+    def execute_command(self, command: str) -> bool:
+
+        try:
+            if os.path.exists(self._path):
+                ...
+            else:
+                raise FileNotFoundError(
+                    f'The folder {self._path} was not found.')
+
+        except FileNotFoundError as error:
+            self.show_error_message(error)
+            return False
+
+        try:
+            subprocess.run(command, capture_output=True, text=True, check=True)
+
+        except subprocess.CalledProcessError as error:
+            self.show_error_message(error)
+            return False
+
+        except PermissionError as error:
+            self.show_error_message(error)
+            return False
+
+        except FileNotFoundError as error:
+            self.show_error_message(error)
+            return False
+
+        return True
+
+    def show_error_message(self, error: Exception):
+        print(f'{ERROR_COLOR}MyTerminal (output): {error} {RESET}')
+
+    def run(self, commands: list[str]) -> bool:
+        if 'create' in commands:
+            if 'venv' in commands:
+                command = create_commands['create_venv'] + commands[2] + ' '
+                if '-p' in commands:
+                    self._path = commands[4]
+                    command = create_commands['create_venv'] + \
+                        self._path + f'/{commands[2]} '
+
+        if self.execute_command(command):
+            return True
+        return False
+
+
+class RunCommands:
+    def __init__(self, commands: list[str]) -> None:
+        self._commands = commands
+        self._terminal = Terminal()
+
+    def run_commands(self) -> bool:
+        if self._terminal.run(self._commands):
+            return True
+        return False
+
+
+while True:
+    command = input(f'{INPUT_COLOR}MyTerminal (input): {RESET}')
+
+    unpack = UnpackingCommands(command)
+    unpacked_commands = unpack.unpacking()
+
+    run_commands = RunCommands(unpacked_commands)
+    print(run_commands.run_commands())
