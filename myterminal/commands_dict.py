fd23cd21f342e5d2a4cc0fda8747a2971b78dae9
Everton-Renan <evertonrenan917@gmail.com>
2025-03-06 18:15:21 -0300

    Create create_file

--- myterminal/commands_dict.py
+++ myterminal/commands_dict.py
@@ -1,5 +1,6 @@
 commands_dict = {
     'create_venv': 'python -m venv ',
     'install_module': '-m pip install ',
-    'activate_venv': '\\Scripts\\python.exe'
+    'activate_venv': '\\Scripts\\python.exe',
+    'create_file': 'powershell -Command New-Item -Path file-path\\file-name -ItemType File'
 }
