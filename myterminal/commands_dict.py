ac2a4135edf2385041c0fc94e0a571399f5993b5
Everton-Renan <evertonrenan917@gmail.com>
2025-03-07 00:28:01 -0300

    Create ls and cd command

--- myterminal/commands_dict.py
+++ myterminal/commands_dict.py
@@ -2,5 +2,7 @@ commands_dict = {
     'create_venv': 'python -m venv ',
     'install_module': '-m pip install ',
     'activate_venv': '\\Scripts\\python.exe',
-    'create_file': 'powershell -Command New-Item -Path file-path\\file-name -ItemType File'
+    'create_file': 'powershell -Command New-Item -Path file-path\\file-name -ItemType File',
+    'ls': 'powershell -Command ls',
+    'cd': 'powershell -Command cd dir'
 }
