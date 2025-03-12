b74fab0669bf503ee568cd710c756e712452d374
Everton-Renan <evertonrenan917@gmail.com>
2025-03-11 22:18:00 -0300

    Create create_dir

--- myterminal/commands_dict.py
+++ myterminal/commands_dict.py
@@ -3,6 +3,7 @@ commands_dict = {
     'install_module': '-m pip install ',
     'activate_venv': '\\Scripts\\python.exe',
     'create_file': 'powershell -Command New-Item -Path file-path\\file-name -ItemType File',
+    'create_dir': 'powershell -Command mkdir name',
     'ls': 'powershell -Command ls',
     'cd': 'powershell -Command cd dir'
 }
