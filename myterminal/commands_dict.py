f8f56931c340f73424e7964a32b4ca7443212178
Everton-Renan <evertonrenan917@gmail.com>
2025-03-16 01:18:18 -0300

    Create commands_dict_linux

--- myterminal/commands_dict.py
+++ myterminal/commands_dict.py
@@ -1,4 +1,4 @@
-commands_dict = {
+commands = {
     'create_venv': 'python -m venv ',
     'install_module': '-m pip install ',
     'activate_venv': '\\Scripts\\python.exe',
@@ -6,4 +6,15 @@ commands_dict = {
     'create_dir': 'powershell -Command mkdir name',
     'ls': 'powershell -Command ls',
     'cd': 'powershell -Command cd dir'
+}
+
+
+commands_dict_linux = {
+    'create_venv': 'python3 -m venv ',
+    'install_module': '-m pip3 install ',
+    'activate_venv': '/bin/activate',
+    'create_file': 'touch file-path/file-name',
+    'create_dir': 'mkdir name',
+    'ls': 'ls',
+    'cd': 'cd dir'
 }
